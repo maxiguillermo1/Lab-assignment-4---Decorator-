d979b4257f05a8e46b3bc2fda5e28e9792d3d3ad
maxi <maxiguillermo@gmail.com>
2024-03-25 13:01:34 -0700

    updatE

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
 # Define a decorator to check authorization before executing a function
+from datetime import datetime
+
 def decorator(func):
     """
     A wrapper function that checks if you're allowed to do something before actually
     doing it. If you're not allowed, it stops and tells you so.
     """
-    def wrapper(*args, **kwargs):
+    def wrapper():
         # Check if you have permission to perform the action
-        if not is_authorized():
+        time = datetime.now().hour
+
+        if 6 <= time < 18:
             # If not authorized, tell the user they can't proceed
-            return "You are not authorized"
+            return func()
         # If authorized, go ahead and perform the action
-        return func(*args, **kwargs)
+        return "You are not authorized"
     return wrapper
 
-# Function that pretends to check if you're allowed to do something
-def is_authorized():
-    """Pretends to check if you have permission to do something. Right now, it always says yes."""
-    # This is where you'd normally check permissions
-    return True
+
 
 # Decorate functions with the authorization check
 # This means "make sure I'm allowed to do this before doing it"
